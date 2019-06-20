bd08a84829d968ae6e937ab06d67b83b33e59fbc
lakshmi <lakshmi.chandrashekara@atmecs.com>
2019-06-20 13:20:44 +0530

    pushing the exception handling project

---
+++ demo/Exceptions/src/com/exception/FileClass.java
@@ -0,0 +1,20 @@
+package com.exception;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class FileClass
+{
+	String path=null;
+	FileClass(String path)
+	{
+		this.path=path;
+	}
+	public void createFile() throws IOException 
+	{
+		File f=new File(path);
+		f.createNewFile();
+		System.out.println("file is created successfully...");
+	}
+}
